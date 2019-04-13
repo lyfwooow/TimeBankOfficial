61caa1295f87f01b041622ec22b0943c3875fc89
ChangFeiyu <changfeiyu@changfeiyudemacbook-pro.local>
2019-04-13 15:52:02 -0400

    Initial Commit

---
+++ TimeBankOfficial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeBankOfficial
+//
+//  Created by ChangFeiyu on 4/13/19.
+//  Copyright © 2019 cis454Group12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
